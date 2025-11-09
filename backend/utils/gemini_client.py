b78411d969a05ab635d258498203bda79f3fba1d
islam Mendjel <126909069+islamMendjel@users.noreply.github.com>
2025-11-09 19:06:49 +0100

    Delete backend directory

--- backend/utils/gemini_client.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-def get_gemini_response(prompt):
-    model = genai.GenerativeModel("gemini-2.5-flash")
-    response = model.generate_content(prompt)
-    return response.text
--- backend/utils/jwt_handler.py
+++
@@ -1,4 +0,0 @@
-from flask_jwt_extended import create_access_token
-
-def generate_token(identity):
-    return create_access_token(identity=identity)
